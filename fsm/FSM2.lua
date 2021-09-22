9d412c50456332980e8d8206a942db2557536899
Blue-Skeptical <jacopo.rizzi@unife.it>
2021-09-22 09:56:57 +0200

    New FSM

--- fsm/FSM2.lua
+++ fsm/FSM2.lua
@@ -9,19 +9,11 @@ return rfsm.state {
     --States
 
     initial = rfsm.conn{},
-
+		Begin = rfsm.state{entry = function() print("Waiting to begin COORSA task.. (Begin_T_E)") end},
 
 	MIR = rfsm.state{
 		initial =rfsm.conn{},
-		--Verso_Prelievo
-		Verso_Prelievo = rfsm.state{entry= function() print("Stato: Verso_Prelievo... waiting for 'Reached_P_E'") end},		
-		Avvio_Prelievo = rfsm.state{ entry = function()	caricate = 0
-								print("Stato: Avvio_Prelievo...") end,},
 
-		--Verso_Deposito
-		Verso_Deposito = rfsm.state{entry= function() print("Stato: Verso_Deposito... waiting for 'Reached_D_E'") end},
-		Avvio_Deposito = rfsm.state{entry= function() print("Stato: Avvio_Deposito... waiting for 'Begin_D_E'") end},
- 
 	   	--Prelievo----------------------------------
 		Prelievo = rfsm.state{
 
@@ -33,7 +25,7 @@ return rfsm.state {
 				--Esegui la detection del piano superiore del palleto e della posizione di ogni pacco
 				Detect_boxes = rfsm.state{entry= function() print("Stato: Detect boxes on pallet") end},
 
-				--Algoritmo di pianificazione per la scelta dei pacchi da prendere	
+				--Algoritmo di pianificazione per la scelta dei pacchi da prendere
 				Choose_boxes = rfsm.state{entry= function() print("Stato: Choose boxes to be collected") end},
 
 				--Muove base e braccio per l'estrazione, altrimenti termina il deposito se non ci sono più box
@@ -50,7 +42,9 @@ return rfsm.state {
 
 	    	},
 
-		Move_Pantografo = rfsm.state{entry = function() print("Stato: MovePantografo") end},
+		Move_Pantografo_P = rfsm.state{entry = function() print("Stato: MovePantografo") end},
+
+		Move_Pantografo_D = rfsm.state{entry = function() print("Stato: MovePantografo") end},
 
 		--Deposito-----------------------------------
 		Deposito = rfsm.state{
@@ -63,7 +57,7 @@ return rfsm.state {
 				--Esegui la detection del piano superiore del palleto e della posizione di ogni pacco
 				Detect_boxes = rfsm.state{entry= function() print("Stato: Detect boxes on pallet") end},
 
-				--Algoritmo di pianificazione per la scelta dei pacchi da prendere	
+				--Algoritmo di pianificazione per la scelta dei pacchi da prendere
 				Choose_boxes = rfsm.state{entry= function() print("Stato: Choose boxes to be collected") end},
 
 				--Muove base e braccio per l'estrazione, altrimenti termina il deposito se non ci sono più box
@@ -79,19 +73,18 @@ return rfsm.state {
 			Local_detection = rfsm.state{entry= function() print("Stato: Local_detection") end},
 
 	    	},
-	
+
 	},
 
 	--TASK FALLITO
 	Task_failed = rfsm.state{entry= function() print("Task fallito") end},
-	
+
 
     --Transitions
-	rfsm.trans{ src = 'initial', tgt='MIR.Verso_Prelievo', pn = 0, events = {'Begin_Task_E'}},
-	rfsm.trans{ src = 'MIR.Verso_Prelievo', tgt='MIR.Avvio_Prelievo', pn = 0, events = {'Reached_P_E'}},
-	--rfsm.trans{ src = 'MIR.Avvio_Prelievo', tgt = 'MIR.Prelievo.Move_to_detect', pn = 0, events = {'begin_p_E'} },
-	rfsm.trans{ src = 'MIR.Avvio_Prelievo' , tgt = 'MIR.Move_Pantografo', pn = 0, events = {'MovePantografo_E'} },
-	rfsm.trans{ src = 'MIR.Move_Pantografo' , tgt = 'MIR.Prelievo.Move_to_detect', pn = 0, events = {'HeightReached_E'} },
+	rfsm.trans{ src = 'initial', tgt='Begin', pn = 0, events = {}},
+	rfsm.trans{ src = 'Begin', tgt='MIR.Move_Pantografo_P', pn = 0, events = {'Begin_T_E'}},
+		--rfsm.trans{ src = 'MIR.Avvio_Prelievo', tgt = 'MIR.Prelievo.Move_to_detect', pn = 0, events = {'begin_p_E'} },
+	rfsm.trans{ src = 'MIR.Move_Pantografo_P' , tgt = 'MIR.Prelievo.Move_to_detect', pn = 0, events = {'HeightReached_E'} },
 	rfsm.trans{ src = 'MIR.Prelievo.Move_to_detect', tgt = 'MIR.Prelievo.DCE.Detect_boxes', pn = 0, events = {'Success_E'} },
 
 	rfsm.trans{ src = 'MIR.Prelievo.DCE.Detect_boxes', tgt = 'MIR.Prelievo.Move_to_detect', pn = 0, events = {'Failure_E'} },
@@ -99,19 +92,14 @@ return rfsm.state {
 	rfsm.trans{ src = 'MIR.Prelievo.DCE.Choose_boxes', tgt = 'MIR.Prelievo.DCE.Move_to_extraction', pn = 0, events = {'Success_E'} },
 	rfsm.trans{ src = 'MIR.Prelievo.DCE.Move_to_extraction', tgt = 'MIR.Prelievo.DCE.Choose_boxes', pn = 0, events = {'Failure_E'} },
 	rfsm.trans{ src = 'MIR.Prelievo.DCE.Move_to_extraction', tgt = 'MIR.Prelievo.Extract_box', pn = 0, events = {'Success_E'} },
-	rfsm.trans{ src = 'MIR.Prelievo.DCE.Move_to_extraction', tgt = 'MIR.Verso_Deposito', pn = 0, events = {'Done_E'} },
+	rfsm.trans{ src = 'MIR.Prelievo.DCE.Move_to_extraction', tgt = 'MIR.Move_Pantografo_D', pn = 0, events = {'Done_E'} },
 	rfsm.trans{ src = 'MIR.Prelievo.DCE', tgt = 'Task_failed', pn = 1, events = {'Complete_Failure_E'} },
 
 	rfsm.trans{ src = 'MIR.Prelievo.Extract_box', tgt = 'MIR.Prelievo.DCE.Move_to_extraction', pn = 0, events = {'Success_E'} },
 	rfsm.trans{ src = 'MIR.Prelievo.Extract_box', tgt = 'MIR.Prelievo.Local_detection', pn = 0, events = {'Failure_E'} },
-	
+	rfsm.trans{ src = 'MIR.Move_Pantografo_D', tgt = 'MIR.Deposito.Move_to_detect', pn=0, events = {'HeightReached_E'}},
 	rfsm.trans{ src = 'MIR.Prelievo.Local_detection', tgt = 'MIR.Prelievo.Extract_box', pn = 0, events = {'Success_E'} },
 	rfsm.trans{ src = 'MIR.Prelievo.Local_detection', tgt = 'MIR.Prelievo.Extract_box', pn = 0, events = {'Failure_E'} },
-
-
-
-	rfsm.trans{ src = 'MIR.Verso_Deposito', tgt = 'MIR.Avvio_Deposito', pn = 0, events = {"Reached_D_E"} },
-	rfsm.trans{ src = 'MIR.Avvio_Deposito', tgt = 'MIR.Deposito.Move_to_detect', pn = 0, events = {"Begin_D_E"} },
 
 	rfsm.trans{ src = 'MIR.Deposito.Move_to_detect', tgt = 'MIR.Deposito.DCE.Detect_boxes', pn = 0, events = {'Success_E'} },
 
@@ -120,12 +108,12 @@ return rfsm.state {
 	rfsm.trans{ src = 'MIR.Deposito.DCE.Choose_boxes', tgt = 'MIR.Deposito.DCE.Move_to_deposit', pn = 0, events = {'Success_E'} },
 	rfsm.trans{ src = 'MIR.Deposito.DCE.Move_to_deposit', tgt = 'MIR.Deposito.DCE.Choose_boxes', pn = 0, events = {'Failure_E'} },
 	rfsm.trans{ src = 'MIR.Deposito.DCE.Move_to_deposit', tgt = 'MIR.Deposito.Deposit_box', pn = 0, events = {'Success_E'} },
-	rfsm.trans{ src = 'MIR.Deposito.DCE.Move_to_deposit', tgt = 'MIR.Verso_Prelievo', pn = 0, events = {'Done_E'} },
+	rfsm.trans{ src = 'MIR.Deposito.DCE.Move_to_deposit', tgt = 'MIR.Move_Pantografo_P', pn = 0, events = {'Done_E'} },
 	rfsm.trans{ src = 'MIR.Deposito.DCE', tgt = 'Task_failed', pn = 1, events = {'Complete_Failure_E'} },
 
 	rfsm.trans{ src = 'MIR.Deposito.Deposit_box', tgt = 'MIR.Deposito.DCE.Move_to_deposit', pn = 0, events = {'Success_E'} },
 	rfsm.trans{ src = 'MIR.Deposito.Deposit_box', tgt = 'MIR.Deposito.Local_detection', pn = 0, events = {'Failure_E'} },
-	
+
 	rfsm.trans{ src = 'MIR.Deposito.Local_detection', tgt = 'MIR.Deposito.Deposit_box', pn = 0, events = {'Success_E'} },
 	rfsm.trans{ src = 'MIR.Deposito.Local_detection', tgt = 'MIR.Deposito.Deposit_box', pn = 0, events = {'Failure_E'} }
 --	rfsm.trans{ src = 'MIR.Deposito.initial', tgt = 'MIR.Deposito.Rileva_Box', pn = 0 },
